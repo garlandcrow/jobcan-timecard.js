c1c0eb127d059b541a39ebb395d8109f23330017
garlandcrow <garlandcrow00@gmail.com>
2018-10-11 09:20:04 +0900

    update to use new jobcan login page

--- src/timecard.js
+++ src/timecard.js
@@ -8,21 +8,19 @@ async function punchTheClock(username, password) {
 
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://ssl.jobcan.jp/login/pc-employee/')
 
-  const sharedLoginButton = 'input[value="ジョブカン共通IDでログイン"]'
-  await page.waitForSelector(sharedLoginButton)
-  await page.click(sharedLoginButton)
-  await page.waitForNavigation()
-  isDebug && console.log('\t> DEBUG: redirected to the jobcan shared login page')
-  isDebug && await page.screenshot({ path: '00_redirected.png' })
+  // goto login page
+  await page.goto('https://id.jobcan.jp/users/sign_in?app_key=atd&redirect_to=https://ssl.jobcan.jp/jbcoauth/callback')
+  isDebug && await page.screenshot({ path: '00_login.png' })
 
+  // enter username and password
   await page.type('#user_email', username)
   const passwordInput = await page.$('#user_password')
   await passwordInput.type(password)
   isDebug && console.log(`\t> DEBUG: entered form data username='${username}', password='${password}'`)
   isDebug && await page.screenshot({ path: '10_data_entered.png' })
 
+  // submit form
   await passwordInput.press('Enter')
   await page.waitForNavigation()
   console.log('\t> logged in user')
@@ -32,9 +30,11 @@ async function punchTheClock(username, password) {
   result.oldStatus = await page.evaluate(() => document.getElementById("working_status").innerText)
   console.log(`\t> current working status: ${result.oldStatus}`)
 
+  // punch the clock
   await page.click('#adit-button-push')
   isDebug && await page.screenshot({ path: '30_clock_clicked.png' })
 
+  // keep watching to see if the status changes (it should if it worked)
   var attempt = 1
   result.timeAwaitingStatusChange = 0
   while (attempt <= maxRetries) {
@@ -42,7 +42,7 @@ async function punchTheClock(username, password) {
     result.timeAwaitingStatusChange += delay
     isDebug && await page.screenshot({ path: `4${attempt}_before_attempt_${attempt}.png` })
 
-    result.status = await page.evaluate(() => document.getElementById("working_status").innerText)
+    result.status = await page.evaluate(() => document.getElementById('working_status').innerText)
 
     if (result.status !== result.oldStatus) {
       isDebug && console.log(`\t> DEBUG: working status changed: ${result.oldStatus} -> ${result.status}`)
